6a01128b80c6fd051a3782b9c40753423b820dc7
samuel.moura1 <samuel.moura@izap.com.br>
2021-01-24 19:16:44 -0300

    feat: add home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
 import * as S from "./styled";
+import { LinkHandler } from "components";
+import PokemonLogo from '../../assets/images/pokemonLogo.png'
 
 const HomePage = () => (
   <S.HomeWrapper>
-    <h1>Home</h1>
+    <S.Container>
+  <LinkHandler url='/map'>
+    <S.Content>
+      <S.Logo src={PokemonLogo} alt='logo da home' />
+      <S.Button>START</S.Button>
+    </S.Content>
+  </LinkHandler>
+    </S.Container>
   </S.HomeWrapper>
 );
 
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,4 +1,44 @@
+import { ResponsiveContainer } from "assets/styles/globalStyled";
 import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--primary-gradient-color);
+`;
+
+export const Container = styled(ResponsiveContainer)``;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+export const Logo = styled.img`
+  margin: 0 0 40px;
+  width: 270px;
+  height: 99px;
+`;
+
+export const Button = styled.button`
+  width: 124px;
+  height: 56px;
+  border: none;
+  border-radius: 42px;
+  background-color: var(--action-color);
+
+  font-size: 18px;
+  line-height: 24px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--action-dark-color);
+  }
 `;
