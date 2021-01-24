a9efa717ce34565acd24613d689f6cf1ab8d43bc
samuel.moura1 <samuel.moura@izap.com.br>
2021-01-24 19:16:11 -0300

    style: add new styles

--- src/assets/styles/globalStyled.js
+++ src/assets/styles/globalStyled.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   * {
@@ -72,5 +72,39 @@ export default createGlobalStyle`
     --primary-dark-color: #004A45;
     --primary-transparent-color:#00D68F ;
     --primary-gradient-color: linear-gradient(90deg, #43E97B 0%, #38F9D7 100%);
+    --action-color: #FF3D71;
+    --action-dark-color: #DB2C66;
+  }
+`;
+
+export const PageContainer = styled.div`
+  padding: var(--navbar-height) 0 0;
+  width: 100%;
+  max-width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ResponsiveContainer = styled.div`
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  position: relative;
+
+  @media (min-width: 480px) {
+    max-width: 728px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
   }
 `;
