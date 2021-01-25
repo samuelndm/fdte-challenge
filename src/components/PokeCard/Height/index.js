f6cc53e602f6d2f0544760a2c3fcfa75fa433a66
samuel.moura1 <samuel.moura@izap.com.br>
2021-01-25 08:27:48 -0300

    fix: small fix at pokecard height

--- src/components/PokeCard/Height/index.js
+++ src/components/PokeCard/Height/index.js
@@ -6,13 +6,13 @@ const Height = ({ height }) => {
   return (
     <S.Stats>
       <S.StatsLabel>ALTURA</S.StatsLabel>
-      <S.StatsContent>{`${height / 10} m`}</S.StatsContent>
+      <S.StatsContent>{`${(height || 0) / 10} m`}</S.StatsContent>
     </S.Stats>
   );
 };
 
 Height.propTypes = {
-  height: PropTypes.string.isRequired,
+  height: PropTypes.number.isRequired,
 };
 
 export default Height;
