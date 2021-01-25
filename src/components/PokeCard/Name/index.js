404094a2670a9d659f0067409625964c1124b236
samuel.moura1 <samuel.moura@izap.com.br>
2021-01-25 10:01:30 -0300

    feat: add editing logic

--- src/components/PokeCard/Name/index.js
+++ src/components/PokeCard/Name/index.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import * as S from "./styled";
 
-const Name = ({ name }) => {
-  return <S.Container>{name || "Nome Indisponível"}</S.Container>;
+const Name = ({ name, isEditable }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <S.Container>
+      <S.Name>
+        {name || "Nome Indisponível"}
+
+        {isEditable && <S.Edit onclick={(e) => setIsEditing(true)} />}
+      </S.Name>
+
+      {isEditing && (
+        <S.EditingContainer>
+          <S.EditingInput type="text" required defaultValue={name} />
+          <S.EditingSave />
+          <S.EditingCancel />
+        </S.EditingContainer>
+      )}
+    </S.Container>
+  );
 };
 
 Name.propTypes = {
   name: PropTypes.string.isRequired,
+  isEditable: PropTypes.bool.isRequired,
 };
 export default Name;
--- src/components/PokeCard/Name/styled.js
+++ src/components/PokeCard/Name/styled.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
+import editIcon from "../../../assets/images/editIcon.png";
 
 export const Container = styled.div`
-  width: 109px;
-  height: 24px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Name = styled.div`
+  padding: 0 30px;
+  position: relative;
+
   font-family: Open Sans;
   font-style: normal;
   font-weight: bold;
@@ -12,3 +22,29 @@ export const Container = styled.div`
   text-transform: uppercase;
   color: var(--neutral-700-color);
 `;
+
+export const Edit = styled.button`
+  width: 16px;
+  height: 16px;
+  position: absolute;
+  margin: auto 0;
+  bottom: 0;
+  top: 0;
+  right: -20px;
+  background: url(${editIcon});
+  border: none;
+  cursor: pointer;
+`;
+
+export const EditingContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const EditingInput = styled.input``;
+
+export const EditingSave = styled.button``;
+
+export const EditingCancel = styled.button``;
